7167389fb5857e37d7f367d363ca886219316326
pramit-marattha <pramitmarattha5497@gmail.com>
2021-12-14 20:28:51 +0545

    MERN roadtrip mapper project added

---
+++ MERN-roadtrip-mapper/server/src/index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+// NOTE morgan is a logger
+const morgan = require("morgan");
+const helmet = require("helmet");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+const middlewares = require("./middlewares");
+const logs = require("./routes/logs.routes.js");
+const app = express();
+
+const DATABASE_CONNECTION = process.env.DATABASE_URL;
+
+mongoose.connect(DATABASE_CONNECTION, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use(morgan("common"));
+app.use(helmet());
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+  })
+);
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Hello There",
+  });
+});
+
+app.use("/api/logs", logs);
+
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log(`Currently Listening at http://localhost:${port}`);
+});
